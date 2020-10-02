65d6ba2f6c8c9eba7d3e46d33dae74b4a61a92a0
aditya278 <aditya.shukla278@gmail.com>
2020-10-02 12:50:35 +0530

    Refactored the index.js file

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const http = require('http');
 const url = require('url');
-const config = require('./config');
 const stringDecoder = require('string_decoder').StringDecoder;
 const port = process.env.PORT || 3000;
 const handlers = require('./lib/handlers');
@@ -8,40 +7,51 @@ const helpers = require("./lib/helpers");
 
 
 const server = http.createServer((req, res) => {
-    const parsedURL = url.parse(req.url, true);
-    const path = parsedURL.pathname;
-    const method = req.method.toLowerCase();
-    const queryStringObject = parsedURL.query;
-    const headers = req.headers;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-    const decoder = new stringDecoder('utf-8');
-    let buffer = '';
-
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
-    });
-    req.on('end', () => {
-        buffer += decoder.end();
-        const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        const data = {
-            "trimmedPath": trimmedPath,
-            "queryStringObject": queryStringObject,
-            "method": method,
-            "headers": headers,
-            "payload": helpers.parseJsonToObject(buffer)
-        }
-        chosenHandler(data, (statusCode, payload) => {
-            statusCode = typeof (statusCode == 'number') ? statusCode : 200;
-            payload = typeof (payload == 'object') ? payload : {};
-            payloadString = JSON.stringify(payload);
+    try{
+        const parsedURL = url.parse(req.url, true);
+        const path = parsedURL.pathname;
+        const method = req.method.toLowerCase();
+        const queryStringObject = parsedURL.query;
+        const headers = req.headers;
+        const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+        const decoder = new stringDecoder('utf-8');
+        let buffer = '';
+    
+        req.on('data', (data) => {
+            buffer += decoder.write(data);
+        });
+        req.on('end', async() => {
+            buffer += decoder.end();
+            const chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+            const data = {
+                trimmedPath,
+                queryStringObject,
+                method,
+                headers,
+                "payload": helpers.parseJsonToObject(buffer)
+            }
+            
+            const response = await chosenHandler(data);
+            if(!response) {
+                res.setHeader('Content-Type', 'application/json');
+                res.writeHead(500);
+                res.end("Server Error");
+            }
+            let statusCode = typeof (response.statusCode == 'number') ? statusCode : 200;
+            let payload = typeof (response.message == 'object' || 'string') ? payload : {};
+            let payloadString = JSON.stringify(payload);
             res.setHeader('Content-Type', 'application/json');
             res.writeHead(statusCode);
             res.end(payloadString);
             console.log(statusCode, payloadString);
         });
-
-    });
-
+    }
+    catch(err){
+        console.error(err);
+        res.setHeader('Content-Type', 'application/json');
+        res.writeHead(500);
+        res.end(err);
+    }
 });
 
 server.listen(port, () => {
